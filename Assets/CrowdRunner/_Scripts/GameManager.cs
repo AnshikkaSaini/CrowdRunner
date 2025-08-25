d1331713dd34a6c251763c2d61efbf439e9b87d2
AnshikkaSaini <110725511+AnshikkaSaini@users.noreply.github.com>
2025-08-25 18:32:03 +0100

    Added Game Manager

---
+++ Assets/CrowdRunner/_Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    private GameState gameState;
+
+    public static Action <GameState> onGameStateChanged;
+
+    public enum GameState
+    {
+        Menu,
+        Game,
+        LevelComplete,
+        GameOver
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+    }
+}
