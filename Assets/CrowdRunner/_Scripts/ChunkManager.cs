aa191ebb925e14ebf648e23788f183f291e7d6c2
AnshikkaSaini <110725511+AnshikkaSaini@users.noreply.github.com>
2025-08-25 17:15:40 +0100

    Finish Line

--- Assets/CrowdRunner/_Scripts/ChunkManager.cs
+++ Assets/CrowdRunner/_Scripts/ChunkManager.cs
@@ -5,11 +5,43 @@ using UnityEngine;
 public class ChunkManager : MonoBehaviour
 {
     //Elements
-    [SerializeField]
-    private Chunk[] chunkPrefabs;
+    [SerializeField] private Chunk[] chunkPrefabs;
+   
+
+    [SerializeField]private Chunk[] levelChunks;
+    
     void Start()
     {
+        CreateOrderedLevel();
         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateOrderedLevel()
+    {
+        Vector3 chunkPosition = Vector3.zero;
+        for (int i = 0; i < levelChunks.Length; i++)
+        {
+            Chunk ChunkToCreate = levelChunks[i];
+            if (i > 0)
+            {
+                chunkPosition.z += ChunkToCreate.GetLength() / 2;
+            }
+            
+            Chunk chunkInstance =  Instantiate (ChunkToCreate, 
+                chunkPosition, Quaternion.identity, transform);
+            
+            chunkPosition.z += chunkInstance.GetLength()/2;
+        }
+    }
+
+    private void CreateRandomLevel()
+    {
         Vector3 chunkPosition = Vector3.zero;
         for (int i = 0; i < 5; i++)
         {
@@ -24,11 +56,7 @@ public class ChunkManager : MonoBehaviour
             
             chunkPosition.z += chunkInstance.GetLength()/2;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
+
 }
--- Assets/CrowdRunner/_Scripts/PlayerDetection.cs
+++ Assets/CrowdRunner/_Scripts/PlayerDetection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDetection : MonoBehaviour
 {
@@ -30,6 +31,11 @@ public class PlayerDetection : MonoBehaviour
 
                 doors.Disable();
                 crowdSystem.ApplyBonus(bonusType, bonusAmount);
+            }
+            
+            else if (dectectedColliders[i].tag == "FinishLine")
+            {
+                SceneManager.LoadScene(0);
             }
         }
     }
